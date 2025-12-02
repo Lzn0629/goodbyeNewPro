605847836a9a0b11347aed07d8a6549c284c8b14
L1zn <prkwh0629@gmail.com>
2025-12-03 00:18:00 +0800

    修改主頁顯示數量

--- goodBuy_web/views/homePage.py
+++ goodBuy_web/views/homePage.py
@@ -15,6 +15,8 @@ from itertools import chain
 from operator import attrgetter
 from goodBuy_shop.models import Shop
 
+# 調整limit可修改首頁顯示數量
+
 def homePage(request):
     # 搜尋關鍵字
     q_raw = request.GET.get('q')
@@ -33,11 +35,11 @@ def homePage(request):
         # 查詢符合關鍵字的 Shop
         # 查詢符合關鍵字的 Want
         if request.user.is_authenticated:
-            shops = personalized_shop_recommendation(request=request, keyword=q, limit=10)
-            wants = personalized_want_recommendation(request=request, keyword=q, limit=10)
-
-        shops = get_hot_shops(request=request, keyword=q, limit=10)
-        wants = get_hot_wants(request=request, keyword=q, limit=10)
+            shops = personalized_shop_recommendation(request=request, keyword=q, limit=80)
+            wants = personalized_want_recommendation(request=request, keyword=q, limit=80)
+        else:
+            shops = get_hot_shops(request=request, keyword=q, limit=80)
+            wants = get_hot_wants(request=request, keyword=q, limit=80)
 
         shops = shopInformation_many(shops)
         wants = wantInformation_many(wants)
@@ -66,13 +68,13 @@ def homePage(request):
 
     #篩選
     if request.user.is_authenticated:
-        shops = personalized_shop_recommendation(request=request, limit=10)
-        wants = personalized_want_recommendation(request=request, limit=10)
+        shops = personalized_shop_recommendation(request=request, limit=80)
+        wants = personalized_want_recommendation(request=request, limit=80)
 
     else:
         # 未登入使用者直接看熱門
-        shops = get_hot_shops(request=request, limit=10)
-        wants = get_hot_wants(request=request, limit=10)
+        shops = get_hot_shops(request=request, limit=80)
+        wants = get_hot_wants(request=request, limit=80)
     
     # 篩選條件：只顯示特定 type（sell / want）
     if post_type == 'sell':
